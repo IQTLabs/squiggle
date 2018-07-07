781cf31365e5d1b52b6c4859f82711b26a37cdc2
Benjamin Lee <benjamindlee@me.com>
2018-07-07 15:52:21 -0700

    Added 3.4 support back!

--- tests/test_yau.py
+++ tests/test_yau.py
@@ -1,8 +1,7 @@
 import pytest
+from pytest import approx
 from hypothesis import given
 from hypothesis.strategies import text
-
-from math import isclose
 
 from squiggle import transform
 
@@ -20,10 +19,8 @@ def test_C():
 
 @given(text(alphabet="ATGC"))
 def test_end_x_value(s):
-    assert isclose(transform(s, method="yau")[0][-1],
-                   ((3**0.5) / 2 * (s.count("C") + s.count("G"))) + (0.5 * (s.count("A") + s.count("T"))))
+    assert transform(s, method="yau")[0][-1] == approx(((3**0.5) / 2 * (s.count("C") + s.count("G"))) + (0.5 * (s.count("A") + s.count("T"))))
 
 @given(text(alphabet="ATGC"))
 def test_end_y_value(s):
-    assert isclose(transform(s, method="yau")[1][-1],
-                   (-(3**0.5) / 2 * s.count("A")) + ((3**0.5) / 2 * s.count("T")) + (0.5 * s.count("C")) + (-0.5 * s.count("G")))
+    assert transform(s, method="yau")[1][-1] == approx((-(3**0.5) / 2 * s.count("A")) + ((3**0.5) / 2 * s.count("T")) + (0.5 * s.count("C")) + (-0.5 * s.count("G")))
