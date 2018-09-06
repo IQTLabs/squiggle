094852270bac45a9f14aa5b1b502c8cdfb83558f
Benjamin Lee <benjamindlee@me.com>
2018-09-05 20:39:00 -0400

    Typo fix

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../squiggle'))
 # -- Project information -----------------------------------------------------
 
 project = 'Squiggle'
-copyright = '2018, In-Q-Tel, Inc.'
+copyright = '2018, In-Q-Tel, Inc'
 author = 'Benjamin Lee'
 
 # The short X.Y version
